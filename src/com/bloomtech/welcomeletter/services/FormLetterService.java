1aec5ce6e052af86aacf05e2695ea60144347d8e
akiracopeland <akiracopeland713@gmail.com>
2024-08-02 21:30:48 -0700

    submitting

--- src/com/bloomtech/welcomeletter/services/FormLetterService.java
+++ src/com/bloomtech/welcomeletter/services/FormLetterService.java
@@ -14,12 +14,14 @@ public class FormLetterService {
 
         //TODO: Replace []ed text with correct info
 
+        String company_name = employee.getCompany().getCompanyname();
         String fullname = employee.getFirstname() + employee.getLastname();
         String startdate = String.valueOf(employee.getStartdate());
         String startingsalary = String.valueOf(employee.getSalary());
-        String company_starttime = String.valueOf(employee.getCompany()) + " " + startdate;
+        String company_starttime =  company_name + " " + startdate;
 
         String replaced_template_data = template_data
+                .replace("[company name]", company_name)
                 .replace("[fullname]", fullname)
                 .replace("[startdate]", startdate)
                 .replace("[startingsalary]", startingsalary)
